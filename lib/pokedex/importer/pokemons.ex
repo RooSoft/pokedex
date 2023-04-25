05f8e0e48a000e8eb3282247475819942629ad43
Marc Lacoursiere <marc@roosoft.com>
2023-04-25 16:05:11 -0400

    added get_name_translations for pokemons

--- lib/pokedex/importer/pokemons.ex
+++ lib/pokedex/importer/pokemons.ex
@@ -15,6 +15,13 @@ defmodule Pokedex.Importer.Pokemons do
     end)
   end
 
+  def get_name_translations(pokedex_id) do
+    Req.get!("https://pokeapi.co/api/v2/pokemon-species/#{pokedex_id}").body["names"] 
+    |> Enum.map(fn %{"name" => name, "language" => %{"name" => language_name}} -> 
+      {language_name, name}
+    end)
+  end
+
   def upsert(pokemons) do
     for {pokedex_id, name} <- pokemons do
       Pokedex.Catalog.upsert_pokemon(%{name: name, pokedex_id: pokedex_id})
