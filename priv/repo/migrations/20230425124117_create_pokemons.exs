1eea1e17389d34fcb4955adecc7a29af4649a4d7
Marc Lacoursiere <marc@roosoft.com>
2023-04-26 09:08:49 -0400

    added names to the pokemons table

--- priv/repo/migrations/20230425124117_create_pokemons.exs
+++ priv/repo/migrations/20230425124117_create_pokemons.exs
@@ -5,8 +5,17 @@ defmodule Pokedex.Repo.Migrations.CreatePokemons do
     create table(:pokemons, primary_key: false) do
       add :id, :integer, primary_key: true
       add :name, :string
+      add :names, :map, default: %{}
 
       timestamps()
     end
+  end
+
+  def up do
+    execute("CREATE INDEX pokemons_names ON pokemons USING GIN(names)")
+  end
+
+  def down do
+    execute("DROP INDEX pokemons_names")
   end
 end
