ec3a712bed1a4b72cdd200f3596417317699fca0
Marc Lacoursiere <marc@roosoft.com>
2023-04-26 14:00:23 -0400

    can now import regions in the database

--- lib/pokedex/importer.ex
+++ lib/pokedex/importer.ex
@@ -1,8 +1,10 @@
 defmodule Pokedex.Importer do
-  alias Pokedex.Importer.Pokemons
+  alias Pokedex.Importer.{PokeAPI, Pokemons, Regions}
 
   def update_all do
     Pokemons.get() |> Pokemons.upsert()
     Pokemons.translate()
+
+    PokeAPI.Regions.get_all |> Regions.upsert()
   end
 end
